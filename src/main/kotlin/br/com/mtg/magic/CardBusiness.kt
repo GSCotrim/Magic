ad72d7c4de63a26ef0da4ea5141ad1a2866ee3db
Giovanni <giocotrim@gmail.com>
2021-10-31 16:22:22 -0300

    Log's first appearance

--- src/main/kotlin/br/com/mtg/magic/CardBusiness.kt
+++ src/main/kotlin/br/com/mtg/magic/CardBusiness.kt
@@ -1,17 +1,21 @@
 package br.com.mtg.magic
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 @Component
 class CardBusiness(
-    @Autowired private var cardRepository: CardRepository
+    @Autowired private var cardRepository: CardRepository,
+    private val log: Logger = LoggerFactory.getLogger(CardBusiness::class.java)
 ) {
     fun getAllCards(): List<Card> {
         val entities = cardRepository.findAll()
         return entities.map { Card.fromEntity(it) }
     }
     fun getCardById(cardId: Long): Card {
+        log.info("Looking for card with id: $cardId")
         val entity = cardRepository.findById(cardId).orElseThrow { CardNotFoundException() }
         return Card.fromEntity(entity)
     }
@@ -20,6 +24,12 @@ class CardBusiness(
         val persistedCard = cardRepository.save(entity)
         return Card.fromEntity(persistedCard)
     }
+
+    // fun nomeDaFuncao(nomeDaVariavel: TipoDaVariavel): TipoDoRetornoDaFuncao {}
+    // TipoDoRetornoDaFuncao nomeDaFuncao(TipoDaVariavel nomeDaVariavel) {}
+    // fun soma(x: int, y: int): int {}
+    // int soma(int x, int y) {}
+
 //    fun blabla(): Card{
 //        cardRepository.findAll()
 //    }
